4921544c7d9ddc1671ad4cb933a53f72f54ad742
Vijaykarthik-S <131847128+Vijaykarthik-S@users.noreply.github.com>
2024-11-04 21:53:54 +0530

    Add files via upload

---
+++ CFDB_practice/Controllers/DepartmentController.cs
@@ -0,0 +1,76 @@
+﻿using CFDB_practice.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CFDB_practice.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly MyContext _context;
+        public DepartmentController(MyContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var departments = _context.Department.ToList();
+            return View(departments);
+        }
+        public IActionResult Details(int id)
+        {
+            var departments = _context.Department.FirstOrDefault(x => x.Id == id);
+            return View(departments);
+        }
+        public IActionResult Edit(int id)
+        {
+            var departments = _context.Department.FirstOrDefault(x => x.Id == id);
+
+            return View(departments);
+
+
+        }
+        [HttpPost]
+        public IActionResult Edit(Department department)
+        {
+            if (department != null)
+            {
+                _context.Entry(department).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Department.Add(department);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int id)
+        {
+            var department = _context.Department.FirstOrDefault(x => x.Id == id);
+            return View(department);
+        }
+        [HttpPost("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var department = _context.Department.FirstOrDefault(x => x.Id == id);
+            if (department != null)
+            {
+                _context.Department.Remove(department);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(department);
+        }
+    }
+}
---
+++ CFDB_practice/Controllers/EmployeeController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CFDB_practice;
+using CFDB_practice.Models;
+
+namespace CFDB_practice.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly MyContext _context;
+        public EmployeeController(MyContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var employees = _context.Employees.ToList();
+            return View(employees);
+        }
+        public IActionResult Details(int id)
+        {
+            var employees = _context.Employees.FirstOrDefault(x => x.EmpId == id);
+            return View(employees);
+        }
+        public IActionResult Edit(int id)
+        {
+            var employees = _context.Employees.FirstOrDefault(x => x.EmpId == id);
+
+            return View(employees);
+
+
+        }
+        [HttpPost]
+        public IActionResult Edit(Employee employee)
+        {
+            if (employee != null)
+            {
+                _context.Entry(employee).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Employees.Add(employee);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int id)
+        {
+            var employee = _context.Employees.FirstOrDefault(x => x.EmpId == id);
+            return View(employee);
+        }
+        [HttpPost("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var employee = _context.Employees.FirstOrDefault(x => x.EmpId == id);
+            if (employee != null)
+            {
+                _context.Employees.Remove(employee);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+    }
+}
+
