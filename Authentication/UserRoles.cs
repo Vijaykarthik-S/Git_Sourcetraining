1ecd0f46c8360ed854a978c9c2d0ad9f20f75c9c
Vijaykarthik-S <131847128+Vijaykarthik-S@users.noreply.github.com>
2024-11-11 14:58:24 +0530

    Delete Authentication directory

--- Authentication/UserRoles.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FastX_Ticket_Booking_System.Authentication
-{
-    public class UserRoles
-    {
-        public const string Admin = "Admin";
-        public const string User = "User";
-        public const string BusOperator = "BusOperator";
-    }
-}
