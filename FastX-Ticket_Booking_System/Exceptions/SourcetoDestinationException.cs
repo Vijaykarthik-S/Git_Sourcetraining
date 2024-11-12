1e70a2fa8feddfaab07900bc62a7db4aadb6ba32
Vijaykarthik-S <131847128+Vijaykarthik-S@users.noreply.github.com>
2024-11-12 17:55:02 +0530

    Add files via upload

---
+++ FastX-Ticket_Booking_System/Exceptions/SourcetoDestinationException.cs
@@ -0,0 +1,8 @@
+﻿namespace FastX_Ticket_Booking_System.Exceptions
+{
+    public class SourcetoDestinationException : Exception
+    {
+        public SourcetoDestinationException(string message) : base(message) { }
+    }
+}
+
