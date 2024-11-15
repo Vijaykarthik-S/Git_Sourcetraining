120951b3c78e9f05ca613286428d8f7123521a90
Vijaykarthik-S <131847128+Vijaykarthik-S@users.noreply.github.com>
2024-11-15 15:26:42 +0530

    Delete FastX-Ticket_Booking_System directory

--- FastX-Ticket_Booking_System/Authentication/ApplicationUser.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace FastX_Ticket_Booking_System.Authentication
-{
-    public class ApplicationUser : IdentityUser
-    {
-    }
-}
--- FastX-Ticket_Booking_System/Exceptions/BookingAlreadyCancelled.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FastX_Ticket_Booking_System.Exceptions
-{
-    public class BookingAlreadyCancelled : Exception
-    {
-         public BookingAlreadyCancelled(string message) : base(message) { }
-        
-    }
-}
