563a9b152c26d8d5a1d4f641e52fc5681c0a8eae
Vijaykarthik-S <131847128+Vijaykarthik-S@users.noreply.github.com>
2024-11-16 15:54:43 +0530

    Delete FastX_Ticket_Booking_System directory

--- FastX_Ticket_Booking_System/Exceptions/InvalidPaymentException.cs
+++
@@ -1,9 +0,0 @@
-﻿
-
-namespace FastX_Ticket_Booking_System.Exceptions
-{
-    public class InvalidPaymentException : Exception
-    {
-        public InvalidPaymentException(string message) : base(message) { }
-    }
-}
--- FastX_Ticket_Booking_System/Exceptions/SourcetoDestinationException.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FastX_Ticket_Booking_System.Exceptions
-{
-    public class SourcetoDestinationException : Exception
-    {
-        public SourcetoDestinationException(string message) : base(message) { }
-    }
-}
